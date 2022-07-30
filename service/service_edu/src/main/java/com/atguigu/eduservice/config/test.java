fff86d6137c22629213b70a18fce9034fd78b0f2
张于宴 <63186145+STAGROUP@users.noreply.github.com>
2022-07-31 04:03:44 +0800

    更新注释

--- service/service_edu/src/main/java/com/atguigu/eduservice/config/test.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/config/test.java
@@ -5,6 +5,7 @@ public class test
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("测试新名字");
+        System.out.println("7.31");
 
 
     }
