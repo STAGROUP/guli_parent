97d128feea953f85d309bcdbcf68a5de1c6d87bb
张于宴 <1441596789@qq.com>
2022-08-01 00:01:06 +0800

    模块

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -2,14 +2,14 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.atguigu.eduservice.service.EduSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -31,6 +31,13 @@ public class EduSubjectController {
     public R addSubject(MultipartFile file){
         //上传过来的excel文件
         subjectService.saveSubject(file,subjectService);
+        return R.ok();
+    }
+    //课程分类列表
+    @GetMapping("getAllSubject")
+    public R getAllSubject(){
+        //list集合泛型是一级分类 因为一级分类有本身也有二级分类集合
+        subjectService.getAllOneTwoSubject();
         return R.ok();
     }
 
