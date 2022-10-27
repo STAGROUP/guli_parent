9f77f1d6fff41e9668a260bd6a88962ab250ecac
张于宴 <1441596789@qq.com>
2022-10-27 23:43:49 +0800

    最大的bug

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/vo/PublishQuery.java
@@ -0,0 +1,8 @@
+package com.atguigu.eduservice.entity.vo;
+import lombok.Data;
+@Data
+public class PublishQuery  {
+    private static final long serialVersionUID = 1L;
+    private String status;
+    private String title;
+}
