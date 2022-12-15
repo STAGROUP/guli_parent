cb4274b91b0940451bd930e4cc5690a99a3687a0
张于宴 <1441596789@qq.com>
2022-12-15 13:16:04 +0800

    msm模块 and banner后台分页查询模块

---
+++ service/service_cms/src/main/java/com/atguigu/educms/config/CmsConfig.java
@@ -0,0 +1,27 @@
+package com.atguigu.educms.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 张于宴
+ * @version 2019.3
+ * @description 配置类
+ * @date 2022/6/28 15:22
+ */
+@Configuration
+@MapperScan("com/atguigu/eduservice/mapper")
+public class CmsConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
