555267aa444a1408900c0f274eafe06cf7f995b7
张于宴 <1441596789@qq.com>
2022-12-16 19:51:15 +0800

    阿里云发送短信2

---
+++ service/service-msm/src/main/java/com/atguigu/msmservice/controller/msmController.java
@@ -0,0 +1,49 @@
+package com.atguigu.msmservice.controller;
+
+import com.atguigu.commonutils.R;
+import com.atguigu.msmservice.service.MsmService;
+import com.atguigu.msmservice.utlis.RandomUtil;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/edumsm/msm")
+@CrossOrigin
+public class msmController {
+    @Autowired
+    private MsmService msmService;
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+
+    //发送短信的方法
+    @GetMapping("/send/{phone}")
+    public R sendMsm(@PathVariable String phone) {
+        //先从redis中获取验证码 如果获取到直接返回
+        String code = redisTemplate.opsForValue().get(phone);
+        if (!StringUtils.isEmpty(code)){
+            return R.ok();
+        }
+        //如果redis获取不到 进行阿里云发送
+        //生成随机的值 传递给阿里云进行发送
+        code = RandomUtil.getFourBitRandom();
+        Map<String, Object> param = new HashMap<>();
+        param.put("code", code);
+        //调用service发送短信的方法
+        boolean isSend = msmService.send(param, phone);
+        if (isSend) {
+            //发送成功把成功的验证码放到redis里面
+            //设置有效时间
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+            return R.ok();
+        } else {
+            return R.error().message("短信发送失败");
+        }
+    }
+}
