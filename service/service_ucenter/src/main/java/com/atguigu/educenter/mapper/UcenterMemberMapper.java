3c10d93d444f1c5fe2c060c859bf91cce4b1c227
张于宴 <1441596789@qq.com>
2022-12-17 12:57:38 +0800

    大修改

---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/mapper/UcenterMemberMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.educenter.mapper;
+
+import com.atguigu.educenter.entity.UcenterMember;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author STA_张于宴
+ * @since 2022-12-17
+ */
+public interface UcenterMemberMapper extends BaseMapper<UcenterMember> {
+
+}
