a9ceb5f148d798d5e155bde4f7c1fd7c2d73cd4e
张于宴 <1441596789@qq.com>
2022-12-17 17:26:03 +0800

    1

---
+++ service/service_uCenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
@@ -0,0 +1,35 @@
+package com.atguigu.educenter.controller;
+
+
+import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.service.UcenterMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.atguigu.commonutils.R;
+
+/**
+ * <p>
+ * 会员表 前端控制器
+ * </p>
+ *
+ * @author STA_张于宴
+ * @since 2022-12-17
+ */
+@RestController
+@RequestMapping("/educenter/member")
+@CrossOrigin
+public class UcenterMemberController {
+    @Autowired
+    private UcenterMemberService memberService;
+    //登录
+    @GetMapping("/longin")
+    public R loginUser(@RequestBody UcenterMember member){
+        String token = memberService.login(member);
+        return R.ok().data("token",token);
+    }
+
+
+    //注册
+
+}
+
