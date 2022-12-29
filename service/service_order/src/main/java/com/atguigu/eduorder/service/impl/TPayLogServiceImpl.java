b3a9f1e138a8eed999ecd9c68dcbecd91eab62f3
张于宴 <1441596789@qq.com>
2022-12-29 11:39:32 +0800

    微信支付接口返回map

--- service/service_order/src/main/java/com/atguigu/eduorder/service/impl/TPayLogServiceImpl.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/service/impl/TPayLogServiceImpl.java
@@ -1,11 +1,21 @@
 package com.atguigu.eduorder.service.impl;
 
+import com.atguigu.eduorder.entity.TOrder;
 import com.atguigu.eduorder.entity.TPayLog;
 import com.atguigu.eduorder.mapper.TPayLogMapper;
+import com.atguigu.eduorder.service.TOrderService;
 import com.atguigu.eduorder.service.TPayLogService;
+import com.atguigu.eduorder.utils.HttpClient;
+import com.atguigu.servicebase.exceptionhandler.GuliExpception;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.wxpay.sdk.WXPayUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,10 +28,50 @@ import java.util.Map;
  */
 @Service
 public class TPayLogServiceImpl extends ServiceImpl<TPayLogMapper, TPayLog> implements TPayLogService {
+    @Autowired
+    private TOrderService orderService;
 
+    //生成微信支付二维码接口
     @Override
     public Map createNatvie(String orderNo) {
-
-        return null;
+        try {
+            //1根据订单查询订单信息
+            QueryWrapper<TOrder> wrapper = new QueryWrapper<>();
+            wrapper.eq("order_No", orderNo);
+            TOrder order = orderService.getOne(wrapper);
+            //2使用map设置生成二维码的参数
+            Map m = new HashMap();
+            m.put("appid", "wx74862e0dfcf69954");
+            m.put("mch_id", "1558950191");
+            //工具类随机生成字符串
+            m.put("nonce_str", WXPayUtil.generateNonceStr());
+            m.put("body", order.getCourseTitle());
+            m.put("out_trade_no", orderNo);
+            m.put("total_fee", order.getTotalFee().multiply(new BigDecimal("100")).longValue() + "");
+            m.put("spbill_create_ip", "127.0.0.1");
+            m.put("notify_url", "http://guli.shop/api/order/weixinPay/weixinNotify\n");
+            m.put("trade_type", "NATIVE");
+            //3发送HTTP client请求 传递参数xml格式 微信支付提供固定的地址
+            HttpClient client = new HttpClient("https://api.mch.weixin.qq.com/pay/unifiedorder");
+            //设置xml格式参数
+            client.setXmlParam(WXPayUtil.generateSignedXml(m, "T6m9iK73b0kn9g5v426MKfHQH7X8rKwb"));
+            client.setHttps(true);
+            //执行请求发送
+            client.post();
+            //4得到发送请求返回结果
+            //返回参数书 是xml格式
+            String content = client.getContent();
+            Map<String, String> resultMap = WXPayUtil.xmlToMap(content);
+            //最终返回数据
+            Map map = new HashMap<>();
+            map.put("out_trade_no", orderNo);
+            map.put("course_id", order.getCourseId());
+            map.put("total_fee", order.getTotalFee());
+            map.put("result_code", resultMap.get("result_code"));//返回二维码操作状态码
+            map.put("code_url", resultMap.get("code_url"));//二维码地址
+            return map;
+        } catch (Exception e) {
+            throw new GuliExpception(20001, "生成二维码失败");
+        }
     }
 }
