71ec5e61811c78f7950efcc4a0f5d45bc32acce9
张于宴 <1441596789@qq.com>
2023-01-01 12:24:13 +0800

    生成统计数据模块初始化

---
+++ service/service_statistics/src/main/java/com/atguigu/staservice/controller/StatisticsDailyController.java
@@ -0,0 +1,23 @@
+package com.atguigu.staservice.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 网站统计日数据 前端控制器
+ * </p>
+ *
+ * @author 张于宴
+ * @since 2023-01-01
+ */
+@RestController
+@RequestMapping("/staservice/sta")
+@CrossOrigin
+public class StatisticsDailyController {
+
+}
+
---
+++ service/service_statistics/src/main/java/com/atguigu/staservice/mapper/StatisticsDailyMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.staservice.mapper;
+
+import com.atguigu.staservice.entity.StatisticsDaily;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 网站统计日数据 Mapper 接口
+ * </p>
+ *
+ * @author 张于宴
+ * @since 2023-01-01
+ */
+public interface StatisticsDailyMapper extends BaseMapper<StatisticsDaily> {
+
+}
