037548e9f064e59bff420258768e3e9bb3a1583e
张于宴 <1441596789@qq.com>
2023-01-01 13:48:35 +0800

    生成统计数据接口简单实现完善

--- service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -1,10 +1,12 @@
 package com.atguigu.staservice.service.impl;
 
 import com.atguigu.commonutils.R;
+import com.atguigu.commonutils.orderVo.UcenterMemberOrder;
 import com.atguigu.staservice.client.UcenterClient;
 import com.atguigu.staservice.entity.StatisticsDaily;
 import com.atguigu.staservice.mapper.StatisticsDailyMapper;
 import com.atguigu.staservice.service.StatisticsDailyService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,12 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
     @Override
     //统计某一天注册人数 生成统计数据
     public void registerCount(String day) {
+        //添加记录之前删除相同日期的数据
+        QueryWrapper<StatisticsDaily> wrapper = new QueryWrapper<StatisticsDaily>();
+        wrapper.eq("date_calculated",day);
+        baseMapper.delete(wrapper);
+
+        //远程掉用
         R registerR = ucenterClient.countRegister(day);
         Integer countRegister = (Integer) registerR.getData().get("countRegister");
 
