3a1a863121ab78d276a0d04ba752f51f11969a5e
张于宴 <1441596789@qq.com>
2023-01-02 10:49:39 +0800

    图表显示接口完成

--- service/service_statistics/src/main/java/com/atguigu/staservice/service/StatisticsDailyService.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/service/StatisticsDailyService.java
@@ -3,6 +3,8 @@ package com.atguigu.staservice.service;
 import com.atguigu.staservice.entity.StatisticsDaily;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 服务类
@@ -15,4 +17,7 @@ public interface StatisticsDailyService extends IService<StatisticsDaily> {
 
     //统计某一天注册人数 生成统计数据
     void registerCount(String day);
+
+    Map<String, Object> getShowData(String type, String begin, String end);
+
 }
--- service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
+++ service/service_statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -12,6 +12,8 @@ import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 /**
  * <p>
  * 网站统计日数据 服务实现类
@@ -50,5 +52,50 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
         statisticsDaily.setCourseNum(courseNum);
         baseMapper.insert(statisticsDaily);
 
+    }
+
+    //图表显示 返回两部分数据 日期json数组 数量json数组
+    @Override
+    public Map<String, Object> getShowData(String type, String begin, String end) {
+       //根据条件查询对应数据
+        QueryWrapper<StatisticsDaily> wrapper = new QueryWrapper<>();
+        wrapper.between("date_calculated",begin,end);
+        wrapper.select("date_calculated",type);
+        List<StatisticsDaily> staList = baseMapper.selectList(wrapper);
+
+        //因为要返回两部分数据 日期 和 日期对应的数量
+        //前端要求数据组json结果对应后端List集合
+        //创建两个list集合 一个日期list 一个数量list
+        List<String> date_calculated = new ArrayList<>();
+        List<Integer> numDataList = new ArrayList<>();
+
+        //遍历查询所有的list集合 进行封装
+        for (int i = 0; i < staList.size(); i++) {
+            StatisticsDaily daily = staList.get(i);
+            //封装日期的集合
+            date_calculated.add(daily.getDateCalculated());
+            //封装对应的数量
+            switch (type) {
+                case "register_num":
+                    numDataList.add(daily.getRegisterNum());
+                    break;
+                case "login_num":
+                    numDataList.add(daily.getLoginNum());
+                    break;
+                case "video_view_num":
+                    numDataList.add(daily.getVideoViewNum());
+                    break;
+                case "course_num":
+                    numDataList.add(daily.getCourseNum());
+                    break;
+                default:
+                    break;
+            }
+        }
+        //把封装好的list集合放到map中返回数据
+        Map<String, Object> map = new HashMap<>();
+        map.put("date_calculated",date_calculated);
+        map.put("numDataList",numDataList);
+        return map;
     }
 }
